4adfe224114a9794fbdf46b8d536a2694d3cfe60
Siddhartha <sidmondal93@gmail.com>
2022-09-20 16:33:26 +0530

    changes is some file

--- springcore/src/main/java/com/springcore/properties/Test.java
+++ springcore/src/main/java/com/springcore/properties/Test.java
@@ -1,5 +1,12 @@
 package com.springcore.properties;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
-    
+    public static void main(String[] args) throws Exception {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("C:/Users/SIDDHARTHA/Documents/Spring Projects/springcore/src/main/java/resources/driver-details.xml");
+        PropertyTest propertyTest =(PropertyTest)applicationContext.getBean("pt");
+        propertyTest.printData();
+    }
 }
